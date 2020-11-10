6f76eff31f1a55d48079f2c7099aae9b5605d5b0
Josef Henline <02josef.henline@gulllakecs.org>
2020-11-10 10:45:14 -0800

    Converted Square to a Function

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -43,6 +43,12 @@ recvThread.start()
 
 # CREATE FUNCTIONS HERE....
 
+# Make a Square Function
+def square():
+    sendmsg('up 50')
+    for i in range(4):
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
 
 print("\nJosef Henline")
 print("Program Name: Tello Training School")
@@ -60,11 +66,9 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Make a Square
-        sendmsg('up 50')
-        for i in range(4):
-            sendmsg('forward 100')
-            sendmsg('ccw 90')
+        # Call the Square Function
+        square()
+
         sendmsg('land')
 
         print('\nGreat Flight!!!')
